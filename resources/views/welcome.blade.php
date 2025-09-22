a8cdfd845f202fa1a8a2253674724e95aff08d87
there <theresa24si@mahasiswa.pcr.ac.id>
2025-09-22 08:21:30 +0700

    undo hapus spasi

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -53,6 +53,7 @@
             <main class="flex max-w-[335px] w-full flex-col-reverse lg:max-w-4xl lg:flex-row">
                 <div class="text-[13px] leading-[20px] flex-1 p-6 pb-12 lg:p-20 bg-white dark:bg-[#161615] dark:text-[#EDEDEC] shadow-[inset_0px_0px_0px_1px_rgba(26,26,0,0.16)] dark:shadow-[inset_0px_0px_0px_1px_#fffaed2d] rounded-bl-lg rounded-br-lg lg:rounded-tl-lg lg:rounded-br-none">
                     <h1 class="mb-1 font-medium">Let's get started</h1>
+                    
                     <!-- Contoh penggunaan route pada tag href -->
                     <a href="{{ route('mahasiswa.show') }}">Klik Disini</a>
                     <p class="mb-2 text-[#706f6c] dark:text-[#A1A09A]">Laravel has an incredibly rich ecosystem. <br>We suggest starting with the following.</p>
