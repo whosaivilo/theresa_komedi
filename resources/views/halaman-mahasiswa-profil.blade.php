c655bdfe84d3908d0103e91e85b2caff1c07d8d9
NazwaSalsabilaHalim <nazwa23tid@mahasiswa.pcr.ac.id>
2025-09-22 08:00:02 +0700

    Project baru

---
+++ resources/views/halaman-mahasiswa-profil.blade.php
@@ -0,0 +1,7 @@
+<html>
+    <body>
+        <h1>Selamat Datang!</h1>
+        <h2>Ini halaman Profil Mahasiswa</h2>
+    </body>
+
+</html>
---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MahasiswaController;
+
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/pcr', function () {
+    return 'Selamat Datang di Website Kampus PCR!'; //tidak kemana-mana, hanya menampilkan teks
+});
+
+
+Route::get('/nama/{param1}', function ($param1) {
+    //{param1} adalah parameter, harus diisi
+    return 'Nama saya: '.$param1;
+});
+
+Route::get('/nim/{param1?}', function ($param1 = '') {
+    //disini {param1?} adalah parameter opsional, boleh diisi boleh tidak
+    //$param1 = '' defaultnya adalah string kosong
+    return 'NIM saya: '.$param1;
+});
+
+Route::get('mahasiswa/{param1}', [MahasiswaController::class, 'show']);
+
+Route::get('/about', function () {
+    return view('halaman-about');
+});
